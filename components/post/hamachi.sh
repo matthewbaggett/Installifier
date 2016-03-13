aa18faa9b177953bbbba3ce302caf99bbd58abd1
Matthew Baggett <matthew@baggett.me>
2016-03-13 00:11:05 +0000

    Adding other things to travis

--- components/post/hamachi.sh
+++ components/post/hamachi.sh
@@ -4,5 +4,3 @@ sleep 3;
 echo "Setting machine nick to $HOSTNAME";
 hamachi set-nick $HOSTNAME
 sleep 3;
-#todo fix this hardcoded account
-hamachi attach matthew@baggett.me;
--- components/pre/mkdirs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ ! -d ~/bin ]
-then
-	mkdir ~/bin;
-fi
