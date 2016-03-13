f699074450a29da70ea9727e3b875013e66d3580
Matthew Baggett <matthew@baggett.me>
2016-03-13 00:26:23 +0000

    oops

--- components/install/slack.sh
+++ components/install/slack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Installing Slack";
 wget -O slack.deb https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-desktop-2.0.1-amd64.deb;
-dpkg -i slack.deb
+sudo dpkg -i slack.deb
