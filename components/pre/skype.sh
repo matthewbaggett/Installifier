ce366dae0eafdfcdcab28f3fd55013d080f5a38a
Matthew Baggett <matthew@baggett.me>
2016-03-13 00:04:27 +0000

    Skype damnit

--- components/pre/skype.sh
+++ components/pre/skype.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+git #!/usr/bin/env bash
 # Install Canonical Partner PPA
 sudo sh -c 'echo "deb http://archive.canonical.com/ubuntu/ wily partner" > /etc/apt/sources.list.d/canonical_partner.list'
