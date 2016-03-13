2bfca04f2af81e26a386fbc8972595448375d595
Matthew Baggett <matthew@baggett.me>
2016-03-13 00:31:53 +0000

    Add Deluge & Quassel, slack should clean itself up

--- components/install/slack.sh
+++ components/install/slack.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Installing Slack";
-wget -O slack.deb https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-desktop-2.0.1-amd64.deb;
-sudo dpkg -i slack.deb
+wget -O /tmp/slack.deb https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-desktop-2.0.1-amd64.deb;
+sudo dpkg -i /tmp/slack.deb
+rm /tmp/slack.deb
