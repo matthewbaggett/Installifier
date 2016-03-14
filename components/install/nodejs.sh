cfc4a68499efe684374faef7ea860f790d892b7b
Matthew Baggett <matthew@baggett.me>
2016-03-14 03:34:11 +0000

    symlink nodejs

--- components/install/nodejs.sh
+++ components/install/nodejs.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing Node & NPM";
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get --force-yes --yes -q install nodejs npm;
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+#sudo ln -s /usr/bin/nodejs /usr/bin/node\
+node -v
