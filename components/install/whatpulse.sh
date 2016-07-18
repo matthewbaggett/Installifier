de0417e66bee7ab889eb4f72591d99adcf6f8ff8
Matthew Baggett <matthew@baggett.me>
2016-07-18 11:02:59 +0100

    update whatpulse path

--- components/install/whatpulse.sh
+++ components/install/whatpulse.sh
@@ -4,7 +4,7 @@ echo "Installing Whatpulse";
 rm -Rf ~/bin/WhatPulse
 rm ~/bin/whatpulse;
 mkdir ~/bin/WhatPulse; cd ~/bin/WhatPulse;
-wget -O whatpulse.tar.gz http://whatpulse.org/downloads/257/64bit/
+wget -O whatpulse.tar.gz http://whatpulse.org/downloads/287/64bit/
 tar -xzf whatpulse.tar.gz;
 rm whatpulse.tar.gz;
 cd -;
