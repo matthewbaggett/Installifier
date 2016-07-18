db2fbaad904c86e92943e7631eba7cf7ec8477ae
Matthew Baggett <matthew@fouroneone.us>
2016-07-18 09:23:35 +0100

    Update phpstorm to 2016-2

--- components/install/phpstorm.sh
+++ components/install/phpstorm.sh
@@ -3,10 +3,10 @@
 echo "Downloading & Unpacking PHPStorm";
 if [ ! -d ~/bin/PhpStorm ]
 then
-	wget https://download.jetbrains.com/webide/PhpStorm-10.0.3.tar.gz -O PhpStorm.tar.gz
+	wget https://download.jetbrains.com/webide/PhpStorm-2016.2.tar.gz;
 	mkdir ~/bin/PhpStorm;
 	tar xzf PhpStorm.tar.gz -C /tmp
-	mv /tmp/PhpStorm-143.1770 /tmp/PhpStorm;
+	mv /tmp/PhpStorm-* /tmp/PhpStorm;
 	mv /tmp/PhpStorm ~/bin/
 	rm PhpStorm.tar.gz;
 	ln -s ~/bin/PhpStorm/bin/phpstorm.sh ~/bin/phpstorm
