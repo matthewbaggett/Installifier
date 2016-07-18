ffca005864d5ac4ff706ef6a6b497ccf11322809
Matthew Baggett <matthew@baggett.me>
2016-07-18 11:02:45 +0100

    update PHPStorm correctly

--- components/install/phpstorm.sh
+++ components/install/phpstorm.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # Install PHPStorm
 echo "Downloading & Unpacking PHPStorm";
-if [ ! -d ~/bin/PhpStorm ]
-then
-	wget https://download.jetbrains.com/webide/PhpStorm-2016.2.tar.gz;
-	mkdir ~/bin/PhpStorm;
-	tar xzf PhpStorm.tar.gz -C /tmp
-	mv /tmp/PhpStorm-* /tmp/PhpStorm;
-	mv /tmp/PhpStorm ~/bin/
-	rm PhpStorm.tar.gz;
-	ln -s ~/bin/PhpStorm/bin/phpstorm.sh ~/bin/phpstorm
-fi
+wget https://download.jetbrains.com/webide/PhpStorm-2016.2.tar.gz -O PhpStorm.tar.gz;
+rm ~/bin/PhpStorm -R;
+mkdir ~/bin/PhpStorm;
+tar xzf PhpStorm.tar.gz -C /tmp
+mv /tmp/PhpStorm-* /tmp/PhpStorm;
+mv /tmp/PhpStorm ~/bin/
+rm PhpStorm.tar.gz;
+rm ~/bin/phpstorm;
+ln -s ~/bin/PhpStorm/bin/phpstorm.sh ~/bin/phpstorm
