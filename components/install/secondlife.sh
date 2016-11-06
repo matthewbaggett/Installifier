d205ccdbfaca766c9a9f2911c872b7f4ab005e23
Matthew Baggett <matthew@baggett.me>
2016-11-06 05:00:19 +0000

    Update secondlife

--- components/install/secondlife.sh
+++ components/install/secondlife.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing Secondlife";
-wget -O sl.tar.xz http://downloads.firestormviewer.org/linux/Phoenix_FirestormOS-Releasex64_x86_64_4.7.5.47975.tar.xz
+wget -O sl.tar.xz http://downloads.firestormviewer.org/linux/Phoenix_FirestormOS-Releasex64_x86_64_4.7.9.50527.tar.xz
 mv sl.tar.xz ~/bin
 cd ~/bin;
 tar xf sl.tar.xz;
