5bb1d9b9c309a773ef90212b6baedd9d8ce0884f
Matthew Baggett <matthew@baggett.me>
2016-11-07 23:10:50 +0000

    steam installer requires python-apt

--- components/install/steam.sh
+++ components/install/steam.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Installing Node & NPM";
+sudo apt-get install -y python-apt;
 wget -O steam.deb https://steamcdn-a.akamaihd.net/client/installer/steam.deb
 sudo dpkg -i steam.deb
