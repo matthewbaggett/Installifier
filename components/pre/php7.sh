8cb288b3c0d1ab766cf7f45bd20bcf6f5d97ae9e
Matthew Baggett <matthew@baggett.me>
2016-11-15 13:59:07 +0000

    Remove PHP7 since its now in xernial proper

--- components/pre/php7.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -yq install wget python-software-properties
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
-sudo add-apt-repository ppa:ondrej/php
